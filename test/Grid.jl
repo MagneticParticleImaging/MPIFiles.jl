8e58aaa096f5c7c5d738254761bbeda0ce0516d5
flo12345678 <f.griese@uke.de>
2017-07-28 15:51:49 +0200

    add grid test

--- test/Grid.jl
+++ test/Grid.jl
@@ -2,6 +2,10 @@ using MPIFiles
 using Base.Test
 using Unitful
 
+rG = RegularGrid{typeof(1.0u"mm")}([3,3,3],[3.0,3.0,3.0]u"mm",[0.0,0.0,0.0]u"mm")
+
+
+
 randPos, randPosTime, numFramesToAcquire, seed = createRandPositions([10.0u"mm"; 10.0u"mm"; 10.0u"mm"], UInt64(100), UInt64(100))
 randPos, randPosTime, numFramesToAcquire, seed = createRandPositions([10.0u"mm"; 10.0u"mm"; 10.0u"mm"], UInt64(100), UInt64(100),
                                                   offset =[10.0u"mm";10.0u"mm";1.0u"mm"], waitLingerTime=0.5u"s",seed=UInt64(10))
